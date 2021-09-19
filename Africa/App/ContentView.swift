f940cf5ca5ab70c4b4d90eb212eb08d1a92a79d3
limonikas@me.com <limonikas@me.com>
2021-09-19 23:39:09 +0300

    Content view multiple view grid layots

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -14,7 +14,27 @@ struct ContentView: View {
     let haptics = UIImpactFeedbackGenerator(style: .medium)
 
     @State private var isGridViewActive: Bool = false
-    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 2)
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Int = 1
+    @State private var toolbarIcon: String = "square.grid.2x2"
+
+    // MARK: - FUNCTIONS
+
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: gridLayout.count % 3 + 1)
+        gridColumn = gridLayout.count
+
+        switch gridColumn {
+        case 1:
+            toolbarIcon = "square.grid.2x2"
+        case 2:
+            toolbarIcon = "square.grid.3x2"
+        case 3:
+            toolbarIcon = "rectangle.grid.1x2"
+        default:
+            toolbarIcon = "square.grid.2x2"
+        }
+    }
 
     // MARK: - BODY
 
@@ -68,8 +88,9 @@ struct ContentView: View {
                             print("Grid view is activated")
                             isGridViewActive = true
                             haptics.impactOccurred()
+                            gridSwitch()
                         }) {
-                            Image(systemName: "square.grid.2x2")
+                            Image(systemName: toolbarIcon)
                                 .font(.title2)
                                 .foregroundColor(isGridViewActive ? .accentColor : .primary)
                         }
