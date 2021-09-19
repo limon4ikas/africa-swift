7c0381715c300333768c213687a67f4cb81bd4a5
limonikas@me.com <limonikas@me.com>
2021-09-19 23:08:16 +0300

    Gallery grid layout

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -8,16 +8,73 @@
 import SwiftUI
 
 struct GalleryView: View {
+    // MARK: - PROPERTIES
+
+    let animals: [AnimalModel] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
+
+    @State private var selectedAnimal: String = "lion"
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Double = 3.0
+
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: Int(gridColumn))
+    }
+
+    // MARK: - BODY
+
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            Text("Gallery")
+            VStack(alignment: .center, spacing: 30) {
+                // MARK: - IMAGE
+
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 8))
+
+                // MARK: - SLIDER
+
+                Slider(value: $gridColumn, in: 2 ... 4, step: 1)
+                    .padding(.horizontal)
+                    .onChange(of: gridColumn, perform: { _ in
+                        gridSwitch()
+                    })
+
+                // MARK: - GRID
+
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                                haptics.impactOccurred()
+                            }
+                    } //: LOOP
+                } //: GRID
+                .animation(.easeIn)
+                .onAppear(perform: {
+                    gridSwitch()
+                })
+            } //: VSTACK
+            .padding(.horizontal, 10)
+            .padding(.vertical, 50)
         } //: SCROLL
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
     }
 }
 
+// MARK: - PREVIEW
+
 struct GalleryView_Previews: PreviewProvider {
+    static let animals: [AnimalModel] = Bundle.main.decode("animals.json")
+
     static var previews: some View {
         GalleryView()
     }
